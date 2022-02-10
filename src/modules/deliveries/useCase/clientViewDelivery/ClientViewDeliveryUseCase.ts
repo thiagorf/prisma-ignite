72cd178bcfdb80b2dc1f452920972362c30497ce
thiagorf <trfarciroli@gmail.com>
2022-02-09 23:46:57 -0300

    finish project

--- src/modules/deliveries/useCase/clientViewDelivery/ClientViewDeliveryUseCase.ts
+++ src/modules/deliveries/useCase/clientViewDelivery/ClientViewDeliveryUseCase.ts
@@ -4,7 +4,7 @@ import prisma from "../../../../client/connection";
 
 export class ClientViewDeliveryUseCase {
 	async execute(id: string) {
-		const clientDeliveries = await prisma.client.findMany({
+		const clientDeliveries = await prisma.client.findFirst({
 			where: {
 				id
 			},
--- src/modules/deliveries/useCase/deliveryManViewDelivery/deliveryManViewDeliveryController.ts
+++ src/modules/deliveries/useCase/deliveryManViewDelivery/deliveryManViewDeliveryController.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from "express";
+import { DeliveryManViewDeliveryUseCase } from "./deliveryManViewDeliveryUseCase";
 
 
 
 export class DeliveryManViewDeliveryController {
 	async handle(request: Request, response: Response) {
 		const { id } = request.user;
+
+		const deliveryManViewDeliveryUseCase = new DeliveryManViewDeliveryUseCase();
+
+		const deliveryManDeliveries = await deliveryManViewDeliveryUseCase.execute(id);
+
+		return response.json(deliveryManDeliveries);
 	}
 }
